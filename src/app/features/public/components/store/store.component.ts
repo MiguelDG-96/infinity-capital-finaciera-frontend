aa4ff386089e6f311ad0db62ae6e4924540c7d53
MiguelDG-96 <mdolicg@gmail.com>
2026-02-12 00:01:00 -0500

    seccion del formulario de store mejorado

--- src/app/features/public/components/store/store.component.ts
+++ src/app/features/public/components/store/store.component.ts
@@ -8,6 +8,7 @@ import { LucideAngularModule, CheckCircle2, ChevronDown } from 'lucide-angular';
   standalone: true,
   imports: [CommonModule, FormsModule, LucideAngularModule],
   templateUrl: './store.component.html',
+  styleUrl: './store.component.css'
 })
 export class StoreComponent {
   readonly CheckCircle2 = CheckCircle2;
